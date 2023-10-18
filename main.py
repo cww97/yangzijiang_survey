dfe5e8f8d7c1145fd119d9cc4875571729827b80
cww97 <cww970329@qq.com>
2023-10-19 01:25:46 +0800

    支持指定答案填写

--- main.py
+++ main.py
@@ -18,18 +18,29 @@ dr = webdriver.Remote(service.service_url, options=options)
 dr.implicitly_wait(10) # seconds
 
 
-def rua_once(url):
-    dr.get(url)
+def rua_once(conf):
+    dr.get(conf['url'])
     dr.refresh()
     time.sleep(0.5)
     js = "document.getElementsByClassName('rt-button--large')[0].style.display=\'block\';"
     dr.execute_script(js)
 
+    idx = 0
     finished = False
     while not finished:
-        dr.find_element(By.CLASS_NAME, "rt-checkbox").click()
+        # 选择选项
+        if not conf['useans']:
+            dr.find_element(By.CLASS_NAME, "rt-checkbox").click()
+        else:
+            options = dr.find_elements(By.CLASS_NAME, "rt-checkbox")
+            for ans in conf['answer'][idx]:
+                ans = min(int(ans), len(options))
+                options[ans - 1].click()
+
         dr.find_element("xpath", "//button").click()
         finished = dr.find_element(By.CLASS_NAME, "rt-button--bottom__text").text == "提交"
+        idx += 1
+        idx = min(idx, len(conf['answer']) - 1)
 
 
 def read_config():
@@ -38,15 +49,19 @@ def read_config():
     if not os.path.exists(file_path):
         raise FileNotFoundError("File 'config.ini' does not exist")
     conn.read(file_path)
-    _url = conn.get('yzj','url')
+    _answer = conn.get('yzj', 'answer').split(',')
+    _answer = [_a.replace(' ', '') for _a in _answer]
+    print(_answer)
     return {
-        'url': _url,
+        'url': conn.get('yzj','url'),
         'repeat': int(conn.get('yzj', 'repeat')),
+        'useans': int(conn.get('yzj', 'useans')) == 1,
+        'answer': _answer,
     }
 
 
 if __name__ == '__main__':
     conf = read_config()
     for _ in tqdm(range(conf['repeat']), desc="Survey: "):
-        rua_once(conf['url'])
+        rua_once(conf)
     dr.quit()
